31945f340ff60d942855f20179fac5d0eade40ef
DESKTOP-3USQG3G\User <hacker10158@gmail.com>
2024-03-07 16:11:48 +0800

    Import hilt, add viewModel

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,4 +1,6 @@
 plugins {
+    kotlin("kapt")
+    id("com.google.dagger.hilt.android")
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
 }
@@ -44,4 +46,13 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation("com.google.dagger:hilt-android:2.44")
+    kapt("com.google.dagger:hilt-android-compiler:2.44")
+    implementation ("androidx.activity:activity-ktx:1.4.1")
+}
+
+// Allow references to generated code
+kapt {
+    correctErrorTypes = true
 }
--- app/src/main/java/com/troy/pokemon/MainActivity.kt
+++ app/src/main/java/com/troy/pokemon/MainActivity.kt
@@ -2,8 +2,15 @@ package com.troy.pokemon
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import com.troy.pokemon.core.MainViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private val mainViewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
