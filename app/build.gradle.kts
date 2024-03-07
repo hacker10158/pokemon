d4f9e9327a958a1a81bfe8f04e7108b72b762f73
DESKTOP-3USQG3G\User <hacker10158@gmail.com>
2024-03-07 23:54:01 +0800

    Fix conflict for hilt and coroutine

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,8 +47,8 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
-    implementation("com.google.dagger:hilt-android:2.44")
-    kapt("com.google.dagger:hilt-android-compiler:2.44")
+    implementation("com.google.dagger:hilt-android:2.46")
+    kapt("com.google.dagger:hilt-android-compiler:2.46")
     implementation ("androidx.activity:activity-ktx:1.4.1")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.9")
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
